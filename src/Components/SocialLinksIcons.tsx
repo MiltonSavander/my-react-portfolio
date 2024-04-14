af9f1d29481b88c5743ecc1443dc66d1403cfa08
Milton <milton.savander@gmail.com>
2024-04-14 17:17:12 +0200

    last path fix

--- src/Components/SocialLinksIcons.tsx
+++ src/Components/SocialLinksIcons.tsx
@@ -1,14 +1,14 @@
+import linkedin_icon from "../../assets/linkedin_icon.svg";
+import github_icon from "../../assets/github_icon.svg";
+import email_icon from "../../assets/email_icon.svg";
+
 const style =
   "size-12 bg-myDarkBlue rounded-full flex items-center justify-center cursor-pointer hover:mt-[-4px] hover:bg-myBlue transition-all ease-in-out";
 export const Linkedin_icon = () => {
   return (
     <div className="size-12">
       <div className={`${style}`}>
-        <img
-          className="size-10"
-          src="./../assets/linkedin_icon.svg"
-          alt="linkedin"
-        />
+        <img className="size-10" src={linkedin_icon} alt="linkedin" />
       </div>
     </div>
   );
@@ -18,11 +18,7 @@ export const Github_icon = () => {
   return (
     <div className="size-12">
       <div className={`${style}`}>
-        <img
-          className="size-10"
-          src="./../assets/github_icon.svg"
-          alt="github"
-        />
+        <img className="size-10" src={github_icon} alt="github" />
       </div>
     </div>
   );
@@ -32,11 +28,7 @@ export const Email_icon = () => {
   return (
     <div className="size-12">
       <div className={`${style}`}>
-        <img
-          className="size-10"
-          src="./../assets/email_icon.svg"
-          alt="email_icon"
-        />
+        <img className="size-10" src={email_icon} alt="email_icon" />
       </div>
     </div>
   );
