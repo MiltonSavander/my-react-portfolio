6455cad086606b558ed298cf2d5de5ea155239a5
Milton <milton.savander@gmail.com>
2024-04-26 14:31:08 +0200

    v1.02 fixed big letter

--- src/Components/CTA.tsx
+++ src/Components/CTA.tsx
@@ -5,7 +5,7 @@ function CTA() {
       className="flex flex-col gap-2 h-[60vh] justify-center items-center"
     >
       <a href="mailto:milton.savander@gmail.com">
-        <button className="cta-button max-sm:scale-[0.85]">kontakta mig</button>
+        <button className="cta-button max-sm:scale-[0.85]">Kontakta mig</button>
       </a>
     </div>
   );
